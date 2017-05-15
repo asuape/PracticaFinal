cb513f439e2b665bce7efaa91fc0d33f15f79b55
Víctor Vanaclocha Cebrián <vicvace@campusaula.com>
2017-05-15 11:04:02 +0200

    15/05/17
    Corrección 11:00. He visto que registro debía ir dentro de Llamada y lo
    he implementado todo en Llamada. Elimino Registro.java

--- src/Llamada.java
+++ src/Llamada.java
@@ -62,4 +62,28 @@ public class Llamada {
 		return "Llamada a " + Agenda.nombre + " finalizada";
 	}
 	
+	public String mostrarRegistro(ArrayList<Llamada> registroLlamadas){
+		
+		//Recorremos el arrayList del registro de llamadas para mostrarlo
+		for (int i = 0; i < registroLlamadas.size(); i++) {
+			
+			System.out.println("-----------------");
+			System.out.println("Registro " + (i+1));
+			System.out.println("Contacto: " + registroLlamadas.get(i).nombre + registroLlamadas.get(i).apellido1 + registroLlamadas.get(i).apellido2);
+			System.out.println("Número de teléfono: " + registroLlamadas.get(i).telefono);
+			System.out.println("Fecha de realización: " + registroLlamadas.get(i).date);
+			
+		}
+				
+		return "Fin del registro.";
+		
+	}
+	
+	public String borrarRegistro(ArrayList<Llamada> registroLlamadas) {
+		
+		registroLlamadas.clear();
+		
+		return "Borrado de registro completado";	
+	}
+	
 }
